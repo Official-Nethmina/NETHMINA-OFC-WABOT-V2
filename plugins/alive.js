9cf75d72de2c27c35012722a6a5629aeb210f4f2
Nethmina Official <nethminaofc.dev@gmail.com>
2026-03-18 20:06:44 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,18 +3,22 @@ const config = require('../config');
 
 cmd({
     pattern: "alive",
+    alias: ["bot","robo","robot"],
+    react: "🎃",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
 },
-async (danuwa, mek, m, {
+async (nethmina, mek, m, {
     from, quoted, body, isCmd, command, args, q, isGroup,
     sender, senderNumber, botNumber2, botNumber, pushname,
     isMe, isOwner, groupMetadata, groupName, participants,
     groupAdmins, isBotAdmins, isAdmins, reply
 }) => {
     try {
-        return await danuwa.sendMessage(from, {
+        await robin.sendPresenceUpdate('recording', from);
+        await robin.sendMessage(from, { audio: { url: "https://github.com/Nethmina-dev/BOT-DATA/raw/refs/heads/main/cmd-voice/alive.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+        return await nethmina.sendMessage(from, {
             image: { url: config.ALIVE_IMG },
             caption: config.ALIVE_MSG
         }, { quoted: mek });
