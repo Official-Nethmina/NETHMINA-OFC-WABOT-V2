f35fa010e2516d54f2f1cbe07599f12d4e6bf956
Nethmina Official <nethminaofc.dev@gmail.com>
2026-03-29 19:36:19 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -12,7 +12,7 @@ cmd({
 async(nethmina, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 await nethmina.sendPresenceUpdate('recording', from);
-await nethmina.sendMessage(from, { audio: { url: "https://github.com/Nethmina-dev/BOT-DATA/raw/refs/heads/main/V-notes/Video%20note%201.mp4" }, mimetype: 'video/mp4', ptv: true }, { quoted: mek });
+await nethmina.sendMessage(from, { video: { url: "https://github.com/Nethmina-dev/BOT-DATA/raw/refs/heads/main/V-notes/Video%20note%201.mp4" }, mimetype: 'video/mp4', ptv: true }, { quoted: mek });
 return await nethmina.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
