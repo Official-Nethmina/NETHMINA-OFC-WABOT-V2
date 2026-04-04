dd6c425beaec42380df3fe008a72a10156378308
Nethmina Official <nethminaofc.dev@gmail.com>
2026-04-04 08:57:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "u24FmDgI#SAuncVZxCZco5HhDlHh-P0PjHHw_WvblCjB1-tN7fPI",
+SESSION_ID: process.env.SESSION_ID || "H6JyiDCK#8pn0XfTW3K99R7Rzq_eKaxG-z6VJ7qFFzGVru6dAmJw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Nethmina-dev/BOT-DATA/blob/main/Logo/ChatGPT%20Image%20Mar%2018,%202026,%2005_47_58%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NETHMINA Is Alive Now😍*",
 BOT_OWNER: '94760860835',  // Replace with the owner's phone number
