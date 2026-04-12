dd3b6828a7f86574dd87716b07e93c52fdf748bb
Nethmina Official <nethminaofc.dev@gmail.com>
2026-04-12 17:19:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ SESSION_ID: process.env.SESSION_ID || "nrZFQB7K#Cx77hrla67pQObs4diihuQEmvvtVvu0B
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Nethmina-dev/BOT-DATA/blob/main/Logo/ChatGPT%20Image%20Mar%2018,%202026,%2005_47_58%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NETHMINA Is Alive Now😍*",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94760860835",  // Replace with the owner's phone number
+OWNER_NAME: process.env.OWNER_NAME || "Bhashitha Nethmina",
+PREFIX: process.env.PREFIX || ".", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
