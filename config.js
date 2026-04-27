51e1830ee2a5ca44045b5cefd95cae32dca3f435
Nethmina Official <nethminaofc.dev@gmail.com>
2026-04-28 00:01:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "u3BBDYBT#VWCW0_iK82XPNlKNx3remYgtBLrpnAK1VlXlszAeamo",
+SESSION_ID: process.env.SESSION_ID || "LyxFXLiJ#0xfLyEMfYSs_2tfqZJ4Z6_EXYiOLqdEJgDXRC9rAuYM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Nethmina-dev/BOT-DATA/blob/main/Logo/ChatGPT%20Image%20Mar%2018,%202026,%2005_47_58%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NETHMINA Is Alive Now😍*",
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94789086040",  // Replace with the owner's phone number
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94760860835",  // Replace with the owner's phone number
 OWNER_NAME: process.env.OWNER_NAME || "Bhashitha Nethmina",
 PREFIX: process.env.PREFIX || ".", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
