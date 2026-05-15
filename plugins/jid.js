dc473d19f9093be0a2d03cd8f825446ce3b05232
Official-Nethmina <nbhashitha159@gmail.com>
2026-05-15 21:17:51 +0530

    Update jid.js

--- plugins/jid.js
+++ plugins/jid.js
@@ -19,20 +19,20 @@ cmd({
             return reply("❌ *Command Restricted* - Only my creator can use this.");
         }
 
-        // 3. JID පිරිසිදු කිරීම
-        const cleanSender = sender.split(":")[0] + "@s.whatsapp.net";
-        const currentChatJid = from.split(":")[0] + (from.endsWith('@g.us') ? "@g.us" : "@s.whatsapp.net");
+       // 3. JID පිරිසිදු කිරීම
+        const cleanSender = sender.split("@")[0] + "@s.whatsapp.net";
+        const cleanChat = from.split("@")[0] + (from.endsWith('@g.us') ? "@g.us" : "@s.whatsapp.net");
 
         if (from.endsWith('@g.us')) {
             // --- Group JID ---
             const sentMsg = await conn.sendMessage(from, { text: "👥 *𝐆𝐑𝐎𝐔𝐏 𝐉𝐈𝐃*" }, { quoted: mek });
-            await conn.sendMessage(from, { text: currentChatJid }, { quoted: sentMsg });
-            } else {
+            await conn.sendMessage(from, { text: cleanChat }, { quoted: sentMsg });
             
+        } else {
             // --- User JID ---
             const sentMsg = await conn.sendMessage(from, { text: "👤 *𝐔𝐒𝐄𝐑 𝐉𝐈𝐃*" }, { quoted: mek });
             await conn.sendMessage(from, { text: cleanSender }, { quoted: sentMsg });
-            }
+        }
 
     } catch (e) {
         console.error("JID Error:", e);
