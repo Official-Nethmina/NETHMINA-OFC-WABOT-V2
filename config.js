7575504752a33f1e999a66617a83c87750302524
Official-Nethmina <nbhashitha159@gmail.com>
2026-07-26 06:15:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "miZxVI6D#y47kK3Ep73zW46EpNAnUnTHN9l5Cqns9wl7GX8MY5K8",
+SESSION_ID: process.env.SESSION_ID || "PypQhBYA#LvFj_CYoInayEG78wszd3upuhy7tUs2LgvK631UPyDo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Nethmina-dev/BOT-DATA/blob/main/Logo/ChatGPT%20Image%20Mar%2018,%202026,%2005_47_58%20PM.png?raw=true",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94760860835",
 OWNER_NAME: process.env.OWNER_NAME || "Bhashitha Nethmina",
